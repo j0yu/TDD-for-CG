82c721eb1ee3ff392f894df718e80866fbb1f586
Joe Yu <josephyu2712+github@gmail.com>
2016-06-18 18:43:11 +0100

    Fixed not raising error

--- ps_maya/psMaya_mainMenu.py
+++ ps_maya/psMaya_mainMenu.py
@@ -6,4 +6,4 @@ import pymel.core as pm
 ## Class to handle and manage the pipeline system's menu within Maya
 class pipelineMenu:
     def __init__(self):
-        NotImplementedError('Add code to create My Menu') 
+        raise NotImplementedError('Add code to create My Menu') 
