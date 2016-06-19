cf19c9c047d42143e5737c88794b80aac2e5eca7
Joe Yu <josephyu2712+github@gmail.com>
2016-06-19 21:21:01 +0100

    main menu implemented

--- ps_maya/psMaya_mainMenu.py
+++ ps_maya/psMaya_mainMenu.py
@@ -5,5 +5,16 @@ import pymel.core as pm
 
 ## Class to handle and manage the pipeline system's menu within Maya
 class pipelineMenu:
-    def __init__(self):
-        raise NotImplementedError('Add code to create My Menu') 
+
+    def __init__(self):      
+        # Name Autodesk gives to main Maya window  
+        mainMayaWindow = pm.ui.PyUI("MayaWindow")
+
+        # Check that it is actually created (just incase this is run before it)
+        if type(mainMayaWindow) == pm.uitypes.Window:
+            # Our menu 
+            self.rootMenu = pm.menu("myMenu", parent=mainMayaWindow)
+            self.rootMenu.setLabel("My Menu")
+        else:
+            pm.warning("WARNING! Maya main window is not initialized yet!")
+            pm.warning("         Pipeline menu is not created")
